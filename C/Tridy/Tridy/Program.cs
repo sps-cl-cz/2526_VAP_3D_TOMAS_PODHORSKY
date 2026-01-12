46a3022a9ac7bf1b32c3a9140999948587f7f8a9
3.D Radovanovič Martin Nenad <Martin.Radovanovic@student.sps-cl.cz>
2026-01-12 11:42:20 +0100

    update

--- C/Tridy/Tridy/Program.cs
+++ C/Tridy/Tridy/Program.cs
@@ -3,22 +3,75 @@ class Program
 {
     static void Main(string[] args)
     {
-        if (args.Length < 3){
+        if (args.Length < 1)
+        {
             Console.WriteLine("Nedostatecny pocet arg");
             return;
         }
         string operation = args[0].ToLower().Trim();
-        string title = args[1];
-        bool completed = args[2].ToLower().Trim() == "splneno";
-        if (operation == "pridat")
+        string[] lines = File.ReadAllLines("tasks.txt");
+        Dictionary<int, MyTask> tasks = ParseFile(lines);
+        switch (operation)
         {
-            string[] lines = File.ReadAllLines("tasks.txt");
-            Dictionary<int, MyTask> tasks = ParseFile(lines);
-            MyTask task = new MyTask(title, completed);
-            tasks.Add(task.Id, task);
-            File.WriteAllLines("tasks.txt", TasksToString(tasks));
-        }
+            case "pridat":
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("Nedostatecny pocet arg");
+                    return;
+                }
+                string title = args[1];
+                bool completed = args[2].ToLower().Trim() == "splneno";
+                MyTask task = new MyTask(title, completed);
+                tasks.Add(task.Id, task);
+                break;
+            case "zobrazit":
+                foreach (MyTask myTask in tasks.Values)
+                {
+                    Console.WriteLine(myTask);
+                }
+                break;
+            case "smazat":
+                try
+                {
+                    if (args.Length < 2)
+                    {
+                        Console.WriteLine("Nedostatecny pocet arg");
+                        return;
+                    }
 
+                    if (tasks.Remove(int.Parse(args[1])))
+                    {
+                        Console.WriteLine("smazano");
+                    }
+                    else
+                    {
+                        Console.WriteLine("neni");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("chyba");
+                }
+                break;
+            case "aktualizovat":
+                if (args.Length < 3)
+                {
+                    Console.WriteLine("Nedostatecny pocet arg");
+                    return;
+                }
+                try
+                {
+                    int id = int.Parse(args[1]);
+                    bool isCompleted = args[2].Trim().ToLower() == "splneno";
+                    MyTask updated = tasks[id];
+                    updated.Completed = isCompleted;
+                    File
+                }
+                break;
+
+
+        }
+        File.WriteAllLines("tasks.txt", TasksToString(tasks));
     }
     // metoda pro prevod slovniku s ulohami na txt retezec
     static List<string> TasksToString(Dictionary<int, MyTask> tasks)
