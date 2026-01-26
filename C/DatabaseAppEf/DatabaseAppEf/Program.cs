b5406bf6749ef0edfc68880d05104347874a5673
3.D Radovanovič Martin Nenad <Martin.Radovanovic@student.sps-cl.cz>
2026-01-26 11:42:15 +0100

    update

---
+++ C/DatabaseAppEf/DatabaseAppEf/Program.cs
@@ -0,0 +1,34 @@
+﻿using DatabaseAppEf;
+
+namespace ConsoleGame
+{
+    public static void Main(string[] args)
+    {
+        string name;
+        if (args.Length == 0)
+        {
+            Console.WriteLine("zadejte sve jemeno.");
+            name = Console.ReadLine();
+        }
+        else
+            name = args[0];
+
+        int score = new Game().Play();
+        using SQLiteDbContext context = new SQLiteDbContext();
+        context.Database.EnsureCreated();
+        Player player = context.Players.FirstOrDefault(p => p.Name == name);
+        if (player == null)
+        {
+            player = new Player()
+            {
+                Name = name,
+                HighScore = score,
+            };
+            context.Players.Add(player);
+        } else
+        {
+            player.HighScore = Math.Max(score, player.HighScore)
+        }
+        context.SaveChanges();
+    }
+}
---
+++ C/DatabaseAppEf/DatabaseAppEf/SQLiteDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseAppEf
+{
+    public class SQLiteDbContext : DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=App.db");
+        }
+    }
+}
