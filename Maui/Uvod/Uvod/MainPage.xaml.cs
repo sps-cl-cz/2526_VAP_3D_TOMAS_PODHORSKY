97fee4698a398c903a79215ff40a5bfeb72c63d9
3.D Radovanovič Martin Nenad <Martin.Radovanovic@student.sps-cl.cz>
2026-02-05 16:27:08 +0100

    Maui

---
+++ Maui/Uvod/Uvod/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace Uvod
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CountLabel = $"Clicked {count} time";
+            else
+                CountLabel = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+
+}
